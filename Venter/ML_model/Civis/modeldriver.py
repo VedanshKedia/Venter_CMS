eda3aa713f973560fdd9f35e588b3b874abef59d
VedanshKedia <kediavedansh@gmail.com>
2019-08-22 15:28:55 +0530

    venter setup

---
+++ Venter/ML_model/Civis/modeldriver.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from . import csvparser, sentencemodel
+
+class SimilarityMapping:
+    '''
+    This class consumes the model and sequences the flow of execution for the given input
+    '''
+    def __init__(self, path):
+        self.filepath = path
+
+    def driver(self):
+        '''
+        '''
+
+        #parsing the input file for having sampled input to the model
+        csvparser.parse(self.filepath)
+        results = sentencemodel.categorizer()
+
+        downloadOutput = pd.ExcelWriter('results.xlsx', engine='xlsxwriter')
+
+        for domain in results:
+            print('Writing Excel for domain %s' % domain)
+            df = pd.DataFrame({ key:pd.Series(value) for key, value in results[domain].items() })
+            df.to_excel(downloadOutput, sheet_name=domain)
+        downloadOutput.save()
